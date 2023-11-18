5f0760d6f52dc5472dcd6d593f37df790bb38aa9
SallYBot <mr.sharkyn@gmail.com>
2023-11-17 16:59:23 -0800

    compile

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,18 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { ALCHEMY_API_URL, ALCHEMY_API_KEY, METAMASK_PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.19",
+  defaultNetwork: "sepolia",
+  networks: {
+     hardhat: {},
+     sepolia: {
+        url: ALCHEMY_API_URL,
+        accounts: [`0x${METAMASK_PRIVATE_KEY}`]
+     }
+  },
 };
