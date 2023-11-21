40749b23d4b2525e6f0520dcb08784ac586ffc7e
SallYBot <mr.sharkyn@gmail.com>
2023-11-20 22:38:39 -0800

    this is it

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 async function main() {
     const SharklerNft = await ethers.getContractFactory("SharklerNft");
-    const sharklerNft = await SharklerNft.deploy("Hello World!");
+    const sharklerNft = await SharklerNft.deploy("Deploying SharklerNft smart contract......");
     console.log("Contract deployed to address:", sharklerNft.address);
  }
  
