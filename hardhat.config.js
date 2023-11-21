ec37597231c3a2b9a40ee992d1047e399c1600d3
SallYBot <mr.sharkyn@gmail.com>
2023-11-20 23:28:15 -0800

    verify the contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,11 @@
 
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 const ALCHEMY_API_URL =  process.env.ALCHEMY_API_URL;
 const METAMASK_PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.8.20",
@@ -17,6 +19,9 @@ module.exports = {
      sepolia: {
         url: ALCHEMY_API_URL,
         accounts: [`0x${METAMASK_PRIVATE_KEY}`]
-     }
+     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   },
 };
